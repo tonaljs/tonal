9d2ff5ba8ee9e1acf4f59b3350e0abecc59512d1
danigb <danigb@gmail.com>
2015-07-18 21:13:55 +0200

    Cycle of fifths

--- cycle-of-fifths.js
+++ cycle-of-fifths.js
@@ -1,9 +1,18 @@
 'use strict'
 
-var CYCLE = 'Abb Ebb Bbb Fb Cb Gb Db Ab Eb Bb F C G D A E B F# C# G# D# A# F'.split(' ')
-var CENTER = 11
+module.exports = cycle
 
-module.exports = function (number) {
-  var number = (+number % 12) + CENTER
-  return CYCLE[number]
+// var CYCLE = 'Abb Ebb Bbb Fb Cb Gb Db Ab Eb Bb F C G D A E B F# C# G# D# A# F'.split(' ')
+var ASC = 'C G D A E B F# C# G# D# A# F'.split(' ')
+var DESC = 'C F Bb Eb Ab Db Gb Cb Fb Bbb Ebb Abb'.split(' ')
+
+function cycle (number, size, reverse) {
+  if (!size) return number < 0 ? DESC[Math.abs(number) % 12] : ASC[number % 12]
+  var result = []
+  var source = reverse ? DESC : ASC
+  if (number < 0) number = 12 + (number % 12)
+  for (var i = number, len = number + size; i < len; i++) {
+    result.push(source[i % 12])
+  }
+  return result
 }
--- test/cycle-of-fifths-test.js
+++ test/cycle-of-fifths-test.js
@@ -4,11 +4,33 @@ var cycle = require('../cycle-of-fifths')
 
 vows.describe('Cycle of fifths').addBatch({
   'positive notes': function () {
-    var notes = '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'.split(' ').map(cycle)
+    var nums = '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16'.split(' ')
+    var notes = nums.map(function (num) {
+      return cycle(+num)
+    })
     assert.equal(notes.join(' '), 'C G D A E B F# C# G# D# A# F C G D A E')
   },
   'negative notes': function () {
-    var notes = '0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16'.split(' ').map(cycle)
+    var nums = '0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -11 -12 -13 -14 -15 -16'.split(' ')
+    var notes = nums.map(function (num) {
+      return cycle(+num)
+    })
     assert.equal(notes.join(' '), 'C F Bb Eb Ab Db Gb Cb Fb Bbb Ebb Abb C F Bb Eb Ab')
+  },
+  'ascendent': function () {
+    assert.equal(cycle(0, 4).join(' '), 'C G D A')
+    assert.equal(cycle(2, 4).join(' '), 'D A E B')
+    assert.equal(cycle(0, 12).join(' '), 'C G D A E B F# C# G# D# A# F')
+    assert.equal(cycle(0, 14).join(' '), 'C G D A E B F# C# G# D# A# F C G')
+    assert.equal(cycle(-1, 7).join(' '), 'F C G D A E B')
+    assert.equal(cycle(7, 4).join(' '), 'C# G# D# A#')
+  },
+  'descendent': function () {
+    assert.equal(cycle(0, 4, true).join(' '), 'C F Bb Eb')
+    assert.equal(cycle(2, 4, true).join(' '), 'Bb Eb Ab Db')
+    assert.equal(cycle(0, 12, true).join(' '), 'C F Bb Eb Ab Db Gb Cb Fb Bbb Ebb Abb')
+    assert.equal(cycle(0, 14, true).join(' '), 'C F Bb Eb Ab Db Gb Cb Fb Bbb Ebb Abb C F')
+    assert.equal(cycle(-1, 7, true).join(' '), 'Abb C F Bb Eb Ab Db')
+    assert.equal(cycle(-5, 2, true).join(' '), 'Cb Fb')
   }
 }).export(module)
