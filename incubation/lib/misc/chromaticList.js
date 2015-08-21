47e2d60158e2da245529c4c6a037387738d17758
danigb <danigb@gmail.com>
2015-08-21 04:34:15 +0200

    chromaticList is now in incubator

---
+++ incubation/lib/misc/chromaticList.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var parse = require('../interval/parse')
+var list = ['P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'A4', 'P5', 'm6', 'M6', 'm7', 'M7']
+
+/**
+ * Returns a interval list with a chromatic scale
+ *
+ * The harmonic chromatic scale is the same whether rising or falling and
+ * includes all the notes in the major, harmonic minor or melodic minor
+ * scales plus flattened second and sharpened fourth degrees
+ *
+ * @param {Integer} length - the number of items in the list
+ * @return {Array} a list (of intervals or notes, depending of params)
+ */
+function chromaticList (length) {
+  if (length > list.length) list = upToOctave(list, length % 12)
+  return list.slice(0, length)
+}
+
+module.exports = chromaticList
+
+function upToOctave (source, octave) {
+  var interval, num
+  var result = source.slice(0, 12)
+  for (var oct = 1; oct <= octave; oct++) {
+    num = oct * 8 - 1
+    for (var i = 0; i < 12; i++) {
+      interval = parse(source[i])
+      result.push(interval.quality + (interval.num + num))
+    }
+  }
+  return result
+}
---
+++ incubation/test/misc/chromaticListTest.js
@@ -0,0 +1,19 @@
+var vows = require('vows')
+var assert = require('assert')
+var chromatic = require('../../lib/list/chromaticList')
+
+vows.describe('list/chromaticList').addBatch({
+  'empty list': function () {
+    assert.deepEqual(chromatic(0), [])
+  },
+  'one octave chromatic interval list': function () {
+    assert.deepEqual(chromatic(1), ['P1'])
+    assert.deepEqual(chromatic(12), ['P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'A4', 'P5', 'm6', 'M6', 'm7', 'M7'])
+  },
+  'two octaves chromatic list': function () {
+    assert.deepEqual(chromatic(13), ['P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'A4', 'P5', 'm6', 'M6', 'm7', 'M7', 'P8'])
+    assert.deepEqual(chromatic(24), [
+      'P1', 'm2', 'M2', 'm3', 'M3', 'P4', 'A4', 'P5', 'm6', 'M6', 'm7', 'M7',
+      'P8', 'm9', 'M9', 'm10', 'M10', 'P11', 'A11', 'P12', 'm13', 'M13', 'm14', 'M14'])
+  }
+}).export(module)
