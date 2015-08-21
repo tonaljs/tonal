e3e80f25c1b0b3698cce481fe3dd69cabd15705c
danigb <danigb@gmail.com>
2015-08-21 06:47:32 +0200

    interval/generic and simplified interval parser

--- test/interval/oppositeTest.js
+++ test/interval/oppositeTest.js
@@ -2,7 +2,7 @@ var vows = require('vows')
 var assert = require('assert')
 var opposite = require('../../lib/interval/opposite')
 
-vows.describe('Interval').addBatch({
+vows.describe('interval/opposite').addBatch({
   'opposite interval': function () {
     assert.equal(opposite('P1'), 'P-1')
     assert.equal(opposite('M2'), 'M-2')
