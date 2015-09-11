9743c0cea9ed6caca2a8321a5125d325160e1071
danigb <danigb@gmail.com>
2015-09-11 02:38:46 +0200

    Better list abstraction

--- test/list/listTest.js
+++ test/list/listTest.js
@@ -1,19 +1,15 @@
 var vows = require('vows')
 var assert = require('assert')
-var pitch = require('../../lib/pitch/pitch')
-var interval = require('../../lib/interval/interval')
 var list = require('../../lib/list/list')
 
 vows.describe('sequence/sequence').addBatch({
-  'create pitch list': function () {
-    assert.deepEqual(list('C D', pitch), ['C', 'D'])
-    assert.deepEqual(list(['C4', 'D'], pitch), ['C4', 'D'])
-    assert.deepEqual(list('C D P1', pitch), null)
+  'create a list with parser': function () {
+    var parser = function (i) { return /^\d+$/.test(i) ? null : i.toUpperCase() }
+    assert.deepEqual(list('C d', parser), ['C', 'D'])
+    assert.deepEqual(list('c 2', parser), null)
   },
-  'create interval list': function () {
-    assert.deepEqual(list('P1 M2', interval), ['P1', 'M2'])
-  },
-  'create a list without validator': function () {
+  'create a list without parser': function () {
     assert.deepEqual(list('A B'), ['A', 'B'])
+    assert.deepEqual(list(['A', 'B']), ['A', 'B'])
   }
 }).export(module)
