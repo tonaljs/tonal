b4c1db85f7d3bd56dae364369acab2867ddb3dd0
danigb <danigb@gmail.com>
2015-12-18 21:17:04 +0100

    Add note-to-array

---
+++ packages/note-to-array/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+var R = require('note-regex')
+var BASES = { C: [0, 0], D: [2, -1], E: [4, -2], F: [-1, 1], G: [1, 0], A: [3, -1], B: [5, -2] }
+
+function memoize (fn) {
+  var cache = {}
+  return function (str) {
+    if (typeof str !== 'string') return null
+    return (str in cache) ? cache[str] : cache[str] = fn(str)
+  }
+}
+/**
+ * Get a pitch in [coord pitch notation](https://github.com/danigb/music.array.notation)
+ * from a string in [scientific pitch notation](https://en.wikipedia.org/wiki/Scientific_pitch_notation)
+ *
+ * The string to parse must be in the form of: `letter[accidentals][octave]`
+ * The accidentals can be up to four # (sharp) or b (flat) or two x (double sharps)
+ *
+ * This function is cached for better performance.
+ *
+ * @name note.parse
+ * @function
+ * @param {String} str - the string to parse
+ * @return {Array} the note in array notation or null if not valid note
+ *
+ * @example
+ * var parse = require('tonal-notation/note.parse')
+ * parse('C') // => [ 0 ]
+ * parse('c#') // => [ 8 ]
+ * parse('c##') // => [ 16 ]
+ * parse('Cx') // => [ 16 ] (double sharp)
+ * parse('Cb') // => [ -6 ]
+ * parse('db') // => [ -4 ]
+ * parse('G4') // => [ 2, 3, null ]
+ * parse('c#3') // => [ 8, -1, null ]
+ */
+module.exports = memoize(function (str) {
+  var m = R.exec(str)
+  if (!m || m[5]) return null
+
+  var base = BASES[m[1].toUpperCase()]
+  var alt = m[2].replace(/x/g, '##').length
+  if (m[2][0] === 'b') alt *= -1
+  var fifths = base[0] + 7 * alt
+  if (!m[3]) return [fifths]
+  var oct = +m[3] + base[1] - 4 * alt
+  var dur = m[4] ? +(m[4].substring(1)) : null
+  return [fifths, oct, dur]
+})
