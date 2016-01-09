002be451c304ecc7c67032aea583a46be455d79d
danigb <danigb@gmail.com>
2016-01-09 21:19:54 +0100

    Remove note/index test

--- packages/music-notation/test/note-test.js
+++ packages/music-notation/test/note-test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 var note = require('../note')
 
 describe('music-notation/note', function () {
-  it('get notes in scientific notation', function () {
+  it.skip('get notes in scientific notation', function () {
     assert.equal(note('fx'), 'F##')
     assert.equal(note('fx/4'), 'F##')
     assert.equal(note('blah'), null)
