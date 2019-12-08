581716ea7621fde22cfb652921cc4f9384424e39
danigb <danigb@gmail.com>
2019-12-08 23:34:44 +0100

    Fix chromatic scale

--- packages/scale-dictionary/data.ts
+++ packages/scale-dictionary/data.ts
@@ -108,6 +108,6 @@ const SCALES: string[][] = [
   ["1P 2M 3m 3M 4P 5d 5P 6M 7m", "composite blues"],
 
   // 12-note scales
-  ["1P 2m 2M 3m 3M 4P 4A 5P 6m 6M 7m 7M", "chromatic"]
+  ["1P 2m 2M 3m 3M 4P 5d 5P 6m 6M 7m 7M", "chromatic"]
 ];
 export default SCALES;
