1e9f5da0537f21ed40f9028d083e6eb0a51d830a
Sasha Mahalia <74146229+sashamahalia@users.noreply.github.com>
2021-07-11 12:33:59 -0700

    Fix Abc notation bug in the 0 octave. (#258)
    * Add 'and' statement in scientificToAbcNotation so that the octave number 0 doesn't return an empty string
    * Write test for octave 0 bug fix
    * Add blank string tests for abcToScientificNotation and scientificToAbcNotation, to bring test coverage to 100%

--- packages/abc-notation/test.ts
+++ packages/abc-notation/test.ts
@@ -17,14 +17,57 @@ describe("@tonaljs/abc-notation", () => {
   });
 
   test("toNote", () => {
-    const ABC = ["__A,,", "_B,", "=C", "d", "^e'", "^^f''", "G,,''", "g,,,'''"];
-    const SCIENTIFIC = ["Abb2", "Bb3", "C4", "D5", "E#6", "F##7", "G4", "G5"];
+    const ABC = [
+      "__A,,",
+      "_B,",
+      "=C",
+      "d",
+      "^e'",
+      "^^f''",
+      "G,,''",
+      "g,,,'''",
+      "",
+    ];
+    const SCIENTIFIC = [
+      "Abb2",
+      "Bb3",
+      "C4",
+      "D5",
+      "E#6",
+      "F##7",
+      "G4",
+      "G5",
+      "",
+    ];
     expect(ABC.map(AbcNotation.abcToScientificNotation)).toEqual(SCIENTIFIC);
   });
 
   test("toAbc", () => {
-    const SCIENTIFIC = ["Abb2", "Bb3", "C4", "D5", "E#6", "F##7", "G#2", "Gb7"];
-    const ABC = ["__A,,", "_B,", "C", "d", "^e'", "^^f''", "^G,,", "_g''"];
+    const SCIENTIFIC = [
+      "Abb2",
+      "Bb3",
+      "C4",
+      "D5",
+      "E#6",
+      "F##7",
+      "G#2",
+      "Gb7",
+      "",
+    ];
+    const ABC = ["__A,,", "_B,", "C", "d", "^e'", "^^f''", "^G,,", "_g''", ""];
+    expect(SCIENTIFIC.map(AbcNotation.scientificToAbcNotation)).toEqual(ABC);
+  });
+  test("toAbc Octave 0", () => {
+    const SCIENTIFIC = ["A0", "Bb0", "C0", "D0", "E#0", "F##0", "G#0"];
+    const ABC = [
+      "A,,,,",
+      "_B,,,,",
+      "C,,,,",
+      "D,,,,",
+      "^E,,,,",
+      "^^F,,,,",
+      "^G,,,,",
+    ];
     expect(SCIENTIFIC.map(AbcNotation.scientificToAbcNotation)).toEqual(ABC);
   });
 });
