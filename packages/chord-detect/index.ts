945e9d845e2d0460595c3874255cb61b96afc34c
Alexander Lichter <github@lichter.io>
2022-11-28 12:50:20 +0100

    chore: refactor

--- packages/chord-detect/index.ts
+++ packages/chord-detect/index.ts
@@ -19,13 +19,16 @@ const namedSet = (notes: string[]) => {
   return (chroma: number) => pcToName[chroma];
 };
 
-export function detect(source: string[], assumePerfectFifth: boolean = false): string[] {
+type DetectOptions = {
+  assumePerfectFifth: boolean
+}
+export function detect(source: string[], options: Partial<DetectOptions>): string[] {
   const notes = source.map((n) => note(n).pc).filter((x) => x);
   if (note.length === 0) {
     return [];
   }
 
-  const found: FoundChord[] = findExactMatches(notes, 1, assumePerfectFifth);
+  const found: FoundChord[] = findExactMatches(notes, 1, options);
 
   return found
     .filter((chord) => chord.weight)
@@ -33,46 +36,52 @@ export function detect(source: string[], assumePerfectFifth: boolean = false): s
     .map((chord) => chord.name);
 }
 
-// 3m 000100000000
-// 3M 000010000000
-// 5d 000000100000
-// 5P 000000010000
-// 5A 000000001000
-// 7m 000000000010
-// 7M 000000000001
-function has3x_5P_7x(chordType: ChordType) {
-  const cromaNum = parseInt(chordType.chroma, 2)
-  return ((cromaNum & 384) && (cromaNum & 16) && (cromaNum & 3))
 
-  /*return (chordType.intervals.some(interval => ['3M', '3m'].includes(interval))) &&
-  (chordType.intervals.some(interval => ['5P'].includes(interval))) &&
-  (chordType.intervals.some(interval => ['7M', '7m'].includes(interval)))*/
+const BITMASK = {
+  // 3m 000100000000
+  // 3M 000010000000
+  anyThirds: 384,
+  // 5P 000000010000
+  perfectFifth: 16,
+  // 5d 000000100000
+  // 5A 000000001000
+  nonPerfectFifths: 40,
+  anySeventh: 3,
 }
 
-function maybeAddPerfectFifth(chroma: string): string {
-  const cromaNum = parseInt(chroma, 2)
-  return (cromaNum & 40)? chroma : (cromaNum | 16).toString(2)
+const testChromaNumber = (bitmask: number) => (chromaNumber: number) => Boolean(chromaNumber & bitmask)
+const hasAnyThird = testChromaNumber(BITMASK.anyThirds)
+const hasPerfectFifth = testChromaNumber(BITMASK.perfectFifth)
+const hasAnySeventh = testChromaNumber(BITMASK.anySeventh)
+const hasNonPerfectFifth = testChromaNumber(BITMASK.nonPerfectFifths)
 
-  /*if(str[6] === '1' || str[8] === '1')
-    return str
-  const ret = str.split('');
-  ret[7] = '1';
-  return ret.join('');*/
+function hasAnyThirdAndPerfectFifthAndAnySeventh(chordType: ChordType) {
+  const chromaNumber = parseInt(chordType.chroma, 2)
+  return hasAnyThird(chromaNumber) && hasPerfectFifth(chromaNumber) && hasAnySeventh(chromaNumber)
 }
 
-function findExactMatches(notes: string[], weight: number, assumePerfectFifth: boolean = false): FoundChord[] {
+function withPerfectFifth(chroma: string): string {
+  const chromaNumber = parseInt(chroma, 2)
+  return hasNonPerfectFifth(chromaNumber)
+  ? chroma 
+  : (chromaNumber | 16).toString(2)
+}
+type FindExactMatchesOptions = {
+  assumePerfectFifth: boolean
+}
+function findExactMatches(notes: string[], weight: number, options: Partial<FindExactMatchesOptions>): FoundChord[] {
   const tonic = notes[0];
   const tonicChroma = note(tonic).chroma;
   const noteName = namedSet(notes);
-  // we need to test all chormas to get the correct baseNote
+  // we need to test all chromas to get the correct baseNote
   const allModes = modes(notes, false);
 
   const found: FoundChord[] = [];
   allModes.forEach((mode, index) => {
-    const assumedMode = maybeAddPerfectFifth(mode)
+    const assumedMode = withPerfectFifth(mode)
     // some chords could have the same chroma but different interval spelling
     const chordTypes = all().filter((chordType) => {
-      if(assumePerfectFifth && has3x_5P_7x(chordType)) {
+      if(options.assumePerfectFifth && hasAnyThirdAndPerfectFifthAndAnySeventh(chordType)) {
         return chordType.chroma === assumedMode
       }
       return chordType.chroma === mode
