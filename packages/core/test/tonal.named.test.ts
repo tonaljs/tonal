65edb0e2d0e87ff7187c8e169fdb636803aa24fc
danigb <danigb@gmail.com>
2023-11-29 21:23:21 +0100

    chore: code cleanup

--- packages/core/test/tonal.named.test.ts
+++ packages/core/test/tonal.named.test.ts
@@ -1,13 +1,13 @@
-import { interval, isNamed, note } from "../index";
+import { interval, isNamedPitch, note } from "../index";
 
 describe("@tonaljs/core", () => {
-  test("isNamed", () => {
-    expect(isNamed(note("C4"))).toBe(true);
-    expect(isNamed(interval("P4"))).toBe(true);
-    expect(isNamed(note("X"))).toBe(true);
+  test("isNamedPitch", () => {
+    expect(isNamedPitch(note("C4"))).toBe(true);
+    expect(isNamedPitch(interval("P4"))).toBe(true);
+    expect(isNamedPitch(note("X"))).toBe(true);
 
-    expect(isNamed(undefined)).toBe(false);
-    expect(isNamed(null)).toBe(false);
-    expect(isNamed("")).toBe(false);
+    expect(isNamedPitch(undefined)).toBe(false);
+    expect(isNamedPitch(null)).toBe(false);
+    expect(isNamedPitch("")).toBe(false);
   });
 });
